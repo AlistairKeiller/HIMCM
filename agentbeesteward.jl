e0dd7839f7c7976138a11daffcb6ae4088d0af01
Alistair Keiller <alistair@keiller.net>
2022-11-11 13:49:05 -0800

    update to have bees as agents instead of colonies

--- agentbeesteward.jl
+++ agentbeesteward.jl
@@ -1,51 +1,43 @@
 import Pkg; Pkg.add("Agents"); Pkg.add("InteractiveDynamics"); Pkg.add("CairoMakie")
 using Agents, Agents.Pathfinding, InteractiveDynamics, CairoMakie
 
-struct bee
-    type::Symbol # egg, larva, pupa, male, worker, queen
-    activity::Symbol # hibernate, nestConstruction, emerging, resting, searching, returningEmpty, returningUnhappyN, returningUnhappyP, nectarForaging, collectNectar, bringingNectar, expForagingN, pollenForaging, collectPollen, bringingPollen, expForagingP, egglaying, nursing
-    emerging_date::Int
-    thEgglaying
-    thForagingNectar
-    thForagingPollen
-end
-
 struct species
     name::Symbol
     seasonStop::Int
 end
 
-@agent colony GridAgent{2} begin
-    species::species
-    bees::Vector{bee}
+struct colony
+    position::Tuple{Int,Int}
 end
+
+@agent bees NoSpaceAgent begin
+    number::Int
+    colony::colony
+    species::species
+    type::Symbol # egg, larva, pupa, male, worker, queen
+    activity::Symbol # hibernate, nestConstruction, emerging, resting, searching, returningEmpty, returningUnhappyN, returningUnhappyP, nectarForaging, collectNectar, bringingNectar, expForagingN, pollenForaging, collectPollen, bringingPollen, expForagingP, egglaying, nursing
+    emerging_date::Int
+end
+
 
 struct flower
     name::Symbol
-    pollen::Float64 # g/flower
-    nectar::Float64 # l/flower
     protein_pollen_prop::Float64
     concentration::Float64 # mol/l
     season::UnitRange{Int}
     corolla_depth::Float64 # m
     inter_flower_time::Float64 # s
-    flower_quantities::Array{Int,2}
+    pollen_production::Array{Float64,2}
+    nectar_production::Array{Float64,2}
     pollen_quantities::Array{Float64,2}
     nectar_quantities::Array{Float64,2}
 end
 
-function colony_step!(colony, model)
-    # seasonal events
+function bee_step!(bee, model)
     # kill all bees that are not hibernating at the end of the season
-    if model.day == colony.species.seasonStop
-        colony.bees = filter(bee -> bee.activity == :hibernate, colony.bees)
+    if model.day == bee.colony.species.seasonStop && bee.activity != :hibernate
+        kill_agent!(bee, model)
     end
-    # kill males in autumn if all queens are in hibernation and no brood is left
-    if !any(bee -> bee.type == :queen && bee.activity != :hibernate, colony.bees) && !any(bee -> bee.type == :egg || bee.type == :larva || bee.type == :pupa, colony.bees)
-        colony.bees = filter(bee -> bee.type != :male, colony.bees)
-    end
-
-
 
     # new queens emerge from hibernation and found new colonies (note: most queens will still be represented as cohorts here!) 
     # Winter survival (survivalProb) is calculated from Beekman et al 1998 (Entomologia Experimentalis et Applicata 89: 207–214, 1998)
@@ -65,8 +57,17 @@ function world_step!(model)
     # update food in tiles
     for flower in model.flowers
         if model.day in flower.season
-            flower.pollen_quantities += flower.flower_quantities * flower.pollen
-            flower.nectar_quantities += flower.flower_quantities * flower.nectar
+            flower.pollen_quantities += flower.pollen_production
+            flower.nectar_quantities += flower.nectar_production
+        end
+    end
+
+    # kill males in autumn if all queens are in hibernation and no brood is left
+    if !any(bee -> bee.type == :queen && bee.activity != :hibernate, model) && !any(bee -> bee.type == :egg || bee.type == :larva || bee.type == :pupa, model)
+        for bee in model
+            if bee.type == :male
+                kill_agent!(bee, model)
+            end
         end
     end
 
@@ -74,12 +75,24 @@ function world_step!(model)
 end
 
 function create_word(
-    flowers::Vector{flower} = [flower(:dandelion, 0.000433333, 0.000470167, 0.000091663467, 1.294673289, 1:364, .0012, 0.6, ones(Int, 100, 100), zeros(100, 100), zeros(100, 100))],
+    flowers::Vector{flower} = [
+        flower(
+            :dandelion,
+            0.091663467,
+            1.294673289,
+            1:364,
+            .0012,
+            0.6,
+            ones(100, 100) * 0.000433333,
+            ones(100, 100) * 0.000000470167,
+            zeros(100, 100),
+            zeros(100, 100)
+        )
+    ],
     land_type::Array{Symbol, 2} = fill(:grass_land, 100, 100),
     tile_size::Float64 = 1.0,
     forging_time::Int = 8*60*60 # seconds per day
 )
-    space = GridSpace(size(land_type))
     properties = Dict(
         :flowers => flowers,
         :land_type => land_type,
@@ -87,7 +100,7 @@ function create_word(
         :forging_time => forging_time,
         :day => 1
     )
-    model = ABM(colony, space; properties)
+    model = ABM(colony, properties)
     return model
 end
 
