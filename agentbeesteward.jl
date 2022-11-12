fa8c430cc33f28edd59434b04602ae3eeab5fbf7
Alistair Keiller <alistair@keiller.net>
2022-11-11 17:27:49 -0800

    add nesting

--- agentbeesteward.jl
+++ agentbeesteward.jl
@@ -2,13 +2,16 @@ import Pkg
 Pkg.add("Agents")
 Pkg.add("InteractiveDynamics")
 Pkg.add("CairoMakie")
-using Agents, Agents.Pathfinding, InteractiveDynamics, CairoMakie
+using Agents, LinearAlgebra
 
 struct Species
     name::Symbol
-    seasonStop::Int
-    devWeight_Q_PupationMin::Float64 # mg
-    devWeight_Q_PupationMax::Float64 # mg
+    season_stop::Int
+    nest_site_food_sources::Vector{Symbol}
+    nest_site_land_types::Vector{Symbol}
+    dev_weight_Q_pupation_min::Float64 # mg
+    dev_weight_Q_pupation_max::Float64 # mg
+    chanceFindNest::Float64
 end
 
 mutable struct Flowers
@@ -28,7 +31,7 @@ mutable struct Colony
     position::Tuple{Int,Int}
 end
 
-@agent Bees NoSpaceAgent begin
+@agent Bees GridAgent{2} begin
     number::Int
     colony::Union{Colony,Nothing}
     species::Species
@@ -46,13 +49,23 @@ function winter_mortality_probibility(bees){
     # However, in 1993 the average initial weight of the queens was highest and in this period the most severe diapause regimes (6 or 8 months) were started. Since the majority of the queens that were given a treatment
     # with a length of 6 or 8 months died, the initial weight distribution of dead queens exceeds that of the surviving queens."
     
-    relative_weight = (bees.weight - bee.species.devWeight_Q_PupationMin) / ( bee.species.devWeight_Q_PupationMax - bee.species.devWeight_Q_PupationMin )
+    relative_weight = (bees.weight - bee.species.dev_weight_Q_pupation_min) / ( bee.species.dev_weight_Q_pupation_max - bee.species.dev_weight_Q_pupation_min )
     survival_probibility = 0.64 / (1 + ℯ ^ (-22 * (relative_weight - 0.32))) # returns survival_probibility
+}
+
+function find_nesting_site(bees, model){
+    # TODO: add a minimum nesting site area
+    # TODO: randomly select a nest site out of possibilities
+    for i ∈ 1:model.sizeof(land_type)[1], j ∈ 1:model.sizeof(land_type)[2]
+        if model.land_type[i,j] ∈ bee.species.nest_site_land_types && norm((i,j)-bees.pos) && any(flowers -> flowers.name ∈ bee.species.nest_site_food_sources && flowers.pollen_production[i,j] != 0, model.flowers)
+            return (i,j)
+        end
+    end
 }
 
 function bees_step!(bees, model)
     # kill all bees that are not hibernating at the end of the season
-    if bees.species.seasonStop == model.tick % 365 && bees.activity != :hibernate
+    if bees.species.season_stop == model.tick % 365 && bees.activity != :hibernate
         kill_agent!(bees, model)
     end
 
@@ -76,6 +89,21 @@ function bees_step!(bees, model)
         # Queen has a risk of dying over winter:
         if rand() < winter_mortality_probibility(bees)
             kill_agent!(bees, model)
+        end
+
+        # AFTER SURVIVAL:
+        bees.activity = :resting
+        bees.colony = nothing
+    end
+    if bees.type == :queen && bees.colony === nothing && bees.activity != :hibernate
+        # searching for nest
+        if rand() < bees.species.chanceFindNest
+            # found nest
+            bees.colony = Colony(find_nesting_site(bees, model))
+        else
+            if rand() < 1 - ((1 - model.mortality_forager) ^ (model.nest_search_time))
+                kill_agent!(bees, model)
+            end
         end
     end
 end
@@ -118,16 +146,21 @@ function create_word(
     ],
     land_type::Array{Symbol,2}=fill(:grass_land, 100, 100),
     tile_size::Float64=1.0, # m
-    forging_time::Int=8 * 60 * 60 # seconds per day
+    forging_time::Int=8 * 60 * 60, # seconds per day
+    nest_search_time::Int=6 * 60 * 60, # seconds
+    mortality_forager::Float=1.0e-5
 )
+    space = GridSpace(sizeof(land_type))
     properties = Dict(
         :flowers => flowers,
         :land_type => land_type,
         :tile_size => tile_size,
         :forging_time => forging_time,
+        :nest_search_time => nest_search_time,
+        :mortality_forager => mortality_forager,
         :tick => 0,
     )
-    model = ABM(Bees, properties)
+    model = ABM(Bees, space ; properties)
     return model
 end
 
